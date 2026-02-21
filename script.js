bcd7253aa5900c1db3d2e20765d4a0bac5527a0e
sharvari0810 <sharvarihaldekar@gmail.com>
2026-02-21 16:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+            function v(val) 
+            {  
+                document.getElementById("s").value +=val;       
+            } 
+             function solution() 
+             {   
+                  document.getElementById("s").value=eval(document.getElementById("s").value); 
+             } 
+              function clr() 
+               {  
+                  document.getElementById("s").value=" ";
+               }
